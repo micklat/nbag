f944bd088bcdb14fb75c3ac2ea922af87584fb1c
Michael Latowicki <mlat@outlook.co.il>
2023-07-28 07:25:29 +0300

    major rewrite: take var names from the definition context rather than from the conversion context

--- src/named_by_assignment.py
+++ src/named_by_assignment.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from collections.abc import Mapping, Iterable, Callable
+from abc import ABC, abstractmethod 
 from typing import Dict
 import sys
 import operator
@@ -6,81 +8,126 @@ import sympy
 import sympy.stats
 
 
-class AbstractReal:
+class AbstractReal(ABC):
 
     def __add__(self, other):
         return Operation(operator.add, (self, other))
     
+    def __radd__(self, other):
+        return Operation(operator.add, (other, self))
+
     def __mul__(self, other):
         return Operation(operator.mul, (self, other))
     
     def __pow__(self, other):
         return Operation(operator.pow, (self, other))
 
+    def __truediv__(self, other):
+        return Operation(operator.truediv, (self, other))
+
+
+class Resolvable(ABC):
+    @abstractmethod
+    def resolve(self):
+        ...
+
+def resolve(x):
+    return x.resolve() if isinstance(x, Resolvable) else x
+
+
+class Missing():
+    pass
+_missing = Missing()
+
 
 @dataclass
-class Operation(AbstractReal):
-    operator: callable
+class Operation(AbstractReal, Resolvable):
+    operator: Callable
     operands: tuple
-    
+    resolved: object = _missing
+
+    def resolve(self):
+        if self.resolved is _missing:
+            self.resolved = self.operator(*map(resolve, self.operands))
+        return self.resolved
+
 
 @dataclass
-class AsYetUnnamed(AbstractReal):
-    constructor: callable
+class AutoNamed(AbstractReal, Resolvable):
+    constructor: Callable
     args: tuple
     kwargs: Dict[str, object]
-    
+    definition_context: Dict[str, object]
+    resolved: object = _missing
+
+    def resolve(self):
+        if self.resolved is _missing:
+            matches = [k for k,v in self.definition_context.items() if v is self]
+            if not matches:
+                raise Exception("no name specified by assignment", self)
+            name = matches[0]
+            self.resolved = self.constructor(
+                    name, *map(resolve, self.args), **generalized_map(resolve, self.kwargs))
+            self.definition_context = None
+        return self.resolved
+
 
 @dataclass
-class NamedByAssignment:
-    constructor: callable
+class Wrapper:
+    constructor: Callable
         
     def __call__(self, *args, **kwargs):
-        return AsYetUnnamed(self.constructor, args, kwargs)
-
+        context = sys._getframe(1).f_locals
+        return AutoNamed(self.constructor, args, kwargs, context)
+    
 
 class DictStruct:
     def __init__(self, **contents):
         self.__dict__ = contents
 
 
-def model(env=None):
-    if env is None:
+def generalized_map(f, x, *args, **kwargs):
+    if isinstance(x, Mapping):
+        return {name: f(v, *args, **kwargs) for (name,v) in x.items()}
+    if isinstance(x, Iterable):
+        return [f(v, *args, **kwargs) for v in x]
+    return f(x, *args, **kwargs)
+
+
+def model(env=_missing):
+    if env is _missing:
         env = sys._getframe(1).f_locals
-    translation = {}
-    for name, unnamed in env.items():
-        if not isinstance(unnamed, AsYetUnnamed):
-            continue
-        translation[id(unnamed)] = unnamed.constructor(name, *unnamed.args, **unnamed.kwargs)
-    def convert(x):
-        key = id(x)
-        if key in translation:
-            return translation[key]
-        if isinstance(x, Operation):
-            x = x.operator(*map(convert, x.operands))
-        # otherwise x is a constant and needs no conversion.
-        translation[key] = x
-        return x
-    for x in env.values():
-        convert(x)
-
-    model = {name: translation[id(v)] for (name,v) in env.items()
-             if (id(v) in translation) and isinstance(v, (AsYetUnnamed, Operation))
-            }
-    return DictStruct(**model)
+        env = {k:v for k,v in env.items() if isinstance(v, (Resolvable, sympy.Basic))}
+    result = generalized_map(resolve, env)
+    if isinstance(result, dict):
+        return DictStruct(**result)
+    return result
 
 
-Normal = NamedByAssignment(sympy.stats.Normal)
+Normal = Wrapper(sympy.stats.Normal)
 def sin(r): return Operation(sympy.sin, (r,)) 
 def cos(r): return Operation(sympy.cos, (r,))
 
+
+def cost():
+    capital_cost = Normal(3,1)
+    operation_cost = 1000
+    total = capital_cost + operation_cost
+    return model()
+
+def benefit():
+    x = Normal(0,1)
+    total = 1 + x ** 2
+    return model()
 
 def test():
     from sympy.stats import sample
     x = Normal(0,1)
     y = x*x
     one = sin(x) ** 2 + cos(x) ** 2
+    ratio = cost().total / benefit().total
     m = model()
+    assert sample(m.ratio) > 0
     print("roughly 0:", sample(m.x, size=10000).mean())
     print("roughly 1:", sample(m.y, size=10000).mean())
     print("exactly 1s:", sample(m.one, size=10))
