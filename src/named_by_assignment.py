7aacc14c1e9aa933de021e44a60f4c49b75e77e5
Michael Latowicki <mlat@outlook.co.il>
2023-07-29 11:09:22 +0300

    remove redundant frame-getting case from assignee_name

--- src/named_by_assignment.py
+++ src/named_by_assignment.py
@@ -3,9 +3,8 @@ from typing import Callable
 import dis, sys, opcode
 
 
-def assignee_name(frame=2):
+def assignee_name(frame=2) -> str:
     if isinstance(frame, int): frame = sys._getframe(frame)
-    elif frame is None: frame = sys._getframe(2)
     for inst in dis.get_instructions(frame.f_code):
         if inst.offset > frame.f_lasti:
             assert opcode.opname[inst.opcode] in ("STORE_FAST", "STORE_GLOBAL", "STORE_NAME", "STORE_ATTR")            
